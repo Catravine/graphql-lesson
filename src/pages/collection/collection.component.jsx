6091abc329bcf9124a6fab09915daebb15a61473
Caroline Courtney <caroline.courtney@earthwavetech.com>
2021-06-20 16:00:39 -0400

    query with variables, shop pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+  console.log('collaction page', collection)
   const { title, items } = collection;
   return (
     <div className='collection-page'>
@@ -21,8 +19,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
